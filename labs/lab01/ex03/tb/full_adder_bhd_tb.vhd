c834c8c5be625b4c1e6983197e5afc151e4f8d58
Nnaemeka Nnachi-Egwu <166872314+Emeka-NE@users.noreply.github.com>
2026-05-17 21:15:16 +0200

    Add files via upload

---
+++ labs/lab01/ex03/tb/full_adder_bhd_tb.vhd
@@ -0,0 +1,72 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity FULL_ADDER_TB is
+end entity;
+
+architecture Behavioral of FULL_ADDER_TB is
+
+    component FULL_ADDER
+        port (
+            A    : in  std_logic;
+            B    : in  std_logic;
+            CIN  : in  std_logic;
+            SUM  : out std_logic;
+            COUT : out std_logic
+        );
+    end component;
+
+    signal A    : std_logic := '0';
+    signal B    : std_logic := '0';
+    signal CIN  : std_logic := '0';
+    signal SUM  : std_logic;
+    signal COUT : std_logic;
+
+begin
+
+    UUT: FULL_ADDER port map (
+        A    => A,
+        B    => B,
+        CIN  => CIN,
+        SUM  => SUM,
+        COUT => COUT
+    );
+
+    process
+    begin
+        -- Test case 1: 0+0+0 = 00
+        A <= '0'; B <= '0'; CIN <= '0';
+        wait for 10 ns;
+
+        -- Test case 2: 0+0+1 = 01
+        A <= '0'; B <= '0'; CIN <= '1';
+        wait for 10 ns;
+
+        -- Test case 3: 0+1+0 = 01
+        A <= '0'; B <= '1'; CIN <= '0';
+        wait for 10 ns;
+
+        -- Test case 4: 0+1+1 = 10
+        A <= '0'; B <= '1'; CIN <= '1';
+        wait for 10 ns;
+
+        -- Test case 5: 1+0+0 = 01
+        A <= '1'; B <= '0'; CIN <= '0';
+        wait for 10 ns;
+
+        -- Test case 6: 1+0+1 = 10
+        A <= '1'; B <= '0'; CIN <= '1';
+        wait for 10 ns;
+
+        -- Test case 7: 1+1+0 = 10
+        A <= '1'; B <= '1'; CIN <= '0';
+        wait for 10 ns;
+
+        -- Test case 8: 1+1+1 = 11
+        A <= '1'; B <= '1'; CIN <= '1';
+        wait for 10 ns;
+
+        wait; -- Stop simulation
+    end process;
+
+end architecture;
